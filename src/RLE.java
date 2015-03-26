e3a2ce724a59075fe66abb892b91e0d28ffb7b8b
kbsko <aleksey-kb@mail.ru>
2015-03-26 15:28:11 +0700

    Add FileInputStream Add Skobki

--- src/RLE.java
+++ src/RLE.java
@@ -1,3 +1,7 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /**
  * Created by Kubish on 26.03.15.
  */
@@ -6,29 +10,43 @@ public class RLE {
         String encode = "";
         char[] charcode = code.toCharArray();
         char currentsymbol = charcode[0];
-        for (int i = 0; i < charcode.length; i++) {
-            if (charcode[i]=='(') charcode[i]='°';
-        }
 
         int count = 1;
         for (int i = 1; i < charcode.length; i++) {
+            // symbol equ
             if (charcode[i] == currentsymbol) {
                 count += 1;
 
+
             } else {
+
+
                 if (count != 1) {
-                    encode = encode + currentsymbol + "(" + count + ")";
-                } else encode = encode + currentsymbol;
+                    encode = encode + currentsymbol + "(" + count + ")";  //many symbol
+
+                } else {
+                    if (currentsymbol == '(') {    // find skobka
+                        encode = encode + currentsymbol + "(" + count + ")";
+
+                    } else encode = encode + currentsymbol;  // one symbol
+                }
                 currentsymbol = charcode[i];
                 count = 1;
 
             }
-
+            // end symbol
             if (i == charcode.length - 1) {
                 if (count != 1) {
                     encode = encode + currentsymbol + "(" + count + ")";
-                } else encode = encode + currentsymbol;
+                } else {
+                    if (currentsymbol == '(') {    // find skobka
+                        encode = encode + currentsymbol + "(" + count + ")";
+
+                    } else encode = encode + currentsymbol;  // one symbol
+
+                }
             }
+
 
         }
         return encode;
@@ -46,50 +64,57 @@ public class RLE {
                 decode = decode + currentsymbol;
                 currentsymbol = charcode[i + 1];
             } else {
-                if (charcode[i+2]!='(')
-                {  
-                    int size = Character.getNumericValue(charcode[i+2]);
-                    int count=1;
-                    while (count<size)
-                    {
-                        decode=decode+currentsymbol;
+                if (charcode[i + 2] != '(') {
+                    int size = Character.getNumericValue(charcode[i + 2]);
+                    int count = 1;
+                    while (count < size) {
+                        decode = decode + currentsymbol;
                         count++;
                     }
-                    i=i+2;
+                    i = i + 2;
 
-                } else
-                {
-                    charcode[i+1]='°';
+                } else {
+                    charcode[i + 1] = '°';
                     i--;
                 }
 
             }
 
+
         }
+
 
         decode = decode + currentsymbol;
 
-       //ekran
+
         char[] chardecode = decode.toCharArray();
-        decode="";
+        decode = "";
         for (int i = 0; i < chardecode.length; i++) {
-            if (chardecode[i]=='°') chardecode[i]='(';
-            decode=decode+chardecode[i];
+            if (chardecode[i] == '°') chardecode[i] = '(';
+            decode = decode + chardecode[i];
         }
 
+        return decode;
+    }
 
-   return decode; }
 
+    public static void main(String argc[]) throws IOException {
 
-    public static void main(String argc[]) {
-        String code = "I am sko(((oblaa( (4)";
-        String encode =encodeRLE(code);
-        System.out.println("Source string:");
-        System.out.println(code);
-        System.out.println("\nEncode string:");
-        System.out.println(encode);
-      String decode=decodeRLE(encode);
-        System.out.println("\nDecode string:");
-      System.out.print(decode);
+        byte[] buffer = new byte[128];
+        BufferedInputStream bufferedInput = new BufferedInputStream(new FileInputStream("G:\\filename.txt"));
+        int bytesRead = 0;
+        while ((bytesRead = bufferedInput.read(buffer)) != -1) {
+            String code = new String(buffer, 0, bytesRead);
+            System.out.println("\nSource string:");
+            System.out.println(code);
+            String encode = encodeRLE(code);
+            System.out.println("\nEncode string:");
+            System.out.println(encode);
+            String decode = decodeRLE(encode);
+            System.out.println("\nDecode string:");
+            System.out.print(decode);
+        }
+        bufferedInput.close();
+
     }
 }
